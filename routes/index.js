e955ecc0f75f95051b1a9a284a07a6cb75b67131
chrisgrazioli <chrisgrazioli@gmail.com>
2018-03-07 15:33:27 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const Item = require('../models/item');
+
+router.get('/', async (req, res, next) => {
+  const items = await Item.find({});
+  res.render('index', {items});
+});
+
+// Add additional routes below:
+router.get('/items/create', (req, res, next)=>{
+  res.render('create');
+});
+
+
+
+module.exports = router;
